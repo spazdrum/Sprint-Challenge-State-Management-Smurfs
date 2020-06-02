e2d340432f474819cc078564a7cac854441ccc00
spazdrum <spazdrum@gmail.com>
2020-06-01 22:27:21 -0400

    import reducer in index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -5,7 +5,8 @@ import App from "./components/App";
 import { Provider } from 'react-redux';
 import thunk from 'react-thunk';
 import { createStore, applyMiddleware } from 'redux';
+import reducer from './reducers/reducer';
 
-const store = createStore(applyMiddleware(thunk));
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
