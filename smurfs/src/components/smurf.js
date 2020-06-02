645e928c2c5f5f0f83e24f4819581cb74370bc0a
spazdrum <spazdrum@gmail.com>
2020-06-01 22:58:11 -0400

    added smurf data

---
+++ smurfs/src/components/smurf.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import smurfCard from './smurefCard';
+import { fetchData } from '../actions/action';
+import { connect } from 'react-redux';
+
+const Smurf = props => {
+    useEffect(() => {
+        props.fetchData()
+    }, []);
+    if (props.isFetching) {
+        return (
+            <div>
+                <h2>Lets find your Smurf!</h2>
+            </div>
+        )
+    }
+    return (
+        <div>
+            {props.err && <p>{ props.err }</p>}
+            {props.smurfData.map(data => (
+                <smurfCard key={data.id} data={data} />
+            ))}
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        smurfData: state.smurfData,
+        isFetching: state.isFetching,
+        error: state.err
+    }
+}
+
+export default connect(mapStateToProps, {fetchData})(Smurf);
