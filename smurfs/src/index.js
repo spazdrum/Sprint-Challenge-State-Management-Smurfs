fe9bd947370650aed39605d4f4a18d1996e65066
spazdrum <spazdrum@gmail.com>
2020-06-01 22:12:49 -0400

    initial commit

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { Provider } from 'react-redux';
+import thunk from 'react-thunk';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(applyMiddleware(thunk));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
